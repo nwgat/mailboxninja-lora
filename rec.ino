e1a523300e7583a7c3753984df8268726f233879
nwgat <github@nwgat.net>
2020-08-10 07:59:35 +0200

    Add files via upload

---
+++ rec.ino
@@ -0,0 +1,64 @@
+// MailboxNinja LoRa Receive
+
+#include <SPI.h>
+#include <LoRa.h>
+#include <ESP8266WiFi.h>
+
+// Lora Pins
+
+#define ss 15
+#define rst 16
+#define dio0 2
+
+void setup() {
+	
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.begin("network-name", "pass-to-network");
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+  Serial.println("LoRa Receiver Callback");
+  LoRa.setPins(ss, rst, dio0);
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+}
+
+  // register the receive callback
+  LoRa.onReceive(onReceive);
+
+  // put the radio into receive mode
+  LoRa.receive();
+}
+
+void loop() {
+  // do nothing
+}
+
+void onReceive(int packetSize) {
+  // received a packet
+  Serial.print("Received packet '");
+
+  // read packet
+  for (int i = 0; i < packetSize; i++) {
+    Serial.print((char)LoRa.read());
+  }
+
+  // print RSSI of packet
+  Serial.print("' with RSSI ");
+  Serial.println(LoRa.packetRssi());
+}
