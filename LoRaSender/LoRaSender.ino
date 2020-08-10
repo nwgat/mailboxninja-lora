ffaf89ef06e9fb3ea55e1c730a648f97a5fce4ba
nwgat <github@nwgat.net>
2020-08-10 14:38:14 +0200

    Add files via upload

---
+++ LoRaSender/LoRaSender.ino
@@ -0,0 +1,38 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+int counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  // setup pins
+  #define ss 15
+  #define rst 32
+  // #define rst 16
+
+  LoRa.setPins(ss, rst);
+  
+  Serial.println("LoRa Sender");
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(5000);
+}
